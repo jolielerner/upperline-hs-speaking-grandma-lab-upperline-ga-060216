9933015a4973252815cb3288457c4a144ea7cba0
Jolie Lerner <jolie.alyse@gmail.com>
2016-06-21 19:19:09 +0000

    Done.

--- lib/grandma.rb
+++ lib/grandma.rb
@@ -1,2 +1,15 @@
-# Write your code here!
+def speak_to_grandma(speech)
+  while speech != "BYE"
+   if speech == speech.upcase
+     year = rand (1930..1950)
+     puts "NO, NOT SINCE #{year}!"
+   else
+     puts "HUH?! SPEAK UP, SONNY!"
+   end
+  speech = gets.chomp
+ end
+end
 
+puts "Speak to Grandma."
+speech = gets.chomp
+speak_to_grandma(speech)
